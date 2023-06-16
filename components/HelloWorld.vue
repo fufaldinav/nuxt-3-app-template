3f4fe3d6965f463e6b13750d6b0b29315530487b
Artem Fufaldin <artem.fufaldin@gmail.com>
2023-06-17 02:06:06 +0500

    chore: install sass

--- components/HelloWorld.vue
+++ components/HelloWorld.vue
@@ -2,9 +2,9 @@
 
 <template>
   <h3 class="text-8xl">
-    <span class="text-white">Hell</span>
-    <span class="text-blue-400">o Wor</span>
-    <span class="text-red-400">ld!!!</span>
+    <span class="text-white drop-shadow">Hell</span>
+    <span class="text-blue-400 drop-shadow">o Wor</span>
+    <span class="text-red-400 drop-shadow">ld!!!</span>
   </h3>
 </template>
 
